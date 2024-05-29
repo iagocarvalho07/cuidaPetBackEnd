72337d6bd3c2299421ae2c65c554b0b4142a85f8
iagocarvalho07 <71970033+iagocarvalho07@users.noreply.github.com>
2024-05-29 08:21:08 -0300

    up

--- cuida_pet_back_end/bin/server.dart
+++ cuida_pet_back_end/bin/server.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:cuida_pet_back_end/application/config/application_config.dart';
+import 'package:cuida_pet_back_end/application/middlewares/cors/cors_middlewares.dart';
 // import 'package:cuida_pet_back_end/controller.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
@@ -16,6 +17,7 @@ void main(List<String> args) async {
   final appConfig = ApplicationConfig();
   appConfig.loadConfigAplication(_router);
   final handler = Pipeline()
+  .addMiddleware(CorsMiddlewares().handler)
   .addMiddleware(logRequests()).addHandler(_router);
   final port = int.parse(Platform.environment['PORT'] ?? '8085');
   final server = await serve(handler, ip, port);
---
+++ cuida_pet_back_end/lib/application/middlewares/cors/cors_middlewares.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'package:cuida_pet_back_end/application/middlewares/Middlewares.dart';
+import 'package:shelf/shelf.dart';
+
+class CorsMiddlewares extends Middlewares {
+  final Map<String, String> header = {
+    'Access-Control-Allow-Origin': "*",
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Header':
+        "${HttpHeaders.contentTypeHeader}, ${HttpHeaders.authorizationHeader}",
+  };
+  @override
+  Future<Response> execute(Request request) async {
+    if (request.method == 'OPTIONS') {
+      return Response(HttpStatus.ok, headers: header);
+    }
+    final responnse = await innerHandler(request);
+    return responnse.change(headers: header);
+  }
+}
